aa914616afe409ccc6f2eb86fe9d40ea4733afb0
Nico Franke <ZerNico.yt@gmail.com>
2019-04-15 18:30:37 +0200

    Customize django admin page for User Model

--- user/admin.py
+++ user/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from user import models
 
-admin.site.register(models.User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'is_staff', 'email', 'date_joined', 'country', 'device']
+    list_filter = ['is_staff', 'date_joined']
+    search_fields = ['username', 'email', 'country', 'device']
+
+
+admin.site.register(models.User, UserAdmin)
