7a88083b695f5fc1f4866e36538e9b26acb8953f
Nico Franke <ZerNico.yt@gmail.com>
2019-06-03 13:51:48 +0200

    Add filtering to OTA api

--- ota/filters.py
+++ ota/filters.py
@@ -3,9 +3,16 @@ from .models import Build
 
 
 class BuildFilter(filters.FilterSet):
+    before_date__lt = filters.NumberFilter(
+        field_name='build_date', lookup_expr='lt',
+        label='Build before (UTC):')
+
+    build_date__gt = filters.NumberFilter(
+        field_name='build_date', lookup_expr='gt',
+        label='Build after (UTC)')
 
     class Meta:
         model = Build
         fields = [
-            'private',
+            'build_type', 'device', 'private', 'version', 'user', 'size'
         ]
