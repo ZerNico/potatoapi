5f08948cbc89f0298111a9508ef341cf079bb305
Nico Franke <ZerNico.yt@gmail.com>
2019-06-04 14:14:13 +0200

    Fix permissions again ¯\_(ツ)_/¯

--- ota/permissions.py
+++ ota/permissions.py
@@ -4,6 +4,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class BuildPermissions(BasePermission):
     """Only allow staff and maintainers to interact with the db"""
     def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
         return request.user.is_staff or \
             obj.user == request.user and \
             request.user.is_maintainer
