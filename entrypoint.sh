783e42f79384dd5e5f966d6575d3be6dc1a5edb0
Nico Franke <ZerNico.yt@gmail.com>
2019-12-01 15:42:02 +0100

    Don't require input

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ sleep 10
 
 # migrations
 python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
 gunicorn potatoapi.wsgi -b 0.0.0.0:8082 --workers 5 --threads 2
 
